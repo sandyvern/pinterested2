805534259ca8ddae1b23aaabbd1ade237a9e8db3
Sandy Hogan <SVern99@aol.com>
2016-07-19 20:08:30 +0000

    added associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702194539) do
+ActiveRecord::Schema.define(version: 20160719193711) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
